ef1627e8a578de2d97fda9ac3e7d3ed3162aba2e
mehakun <mehakun1@gmail.com>
2016-11-18 16:20:30 +0300

    remade 3rd lab

--- OpSys/3lab/main.c
+++ OpSys/3lab/main.c
@@ -4,61 +4,85 @@
 
 typedef struct TSmth {
   int* deck;
-  int equalCount;
+  int isEqual;
   int iterations;
-  pthread_mutex_t mutex;
-}TSmth;
+} TSmth;
 
 void* randr(void* arg) {
-  pthread_mutex_lock(&((TSmth*)arg)->mutex);
   double scaled;
-  
-  for (int i = 0; i < ((TSmth*)arg)->iterations;i++) {
+
+  for (int i = 0; i < ((TSmth*)arg)->iterations; i++) {
     scaled = (double)rand()/RAND_MAX;
     ((TSmth*)arg)->deck[i] = 52*scaled;
-  }
-  
-  pthread_mutex_unlock(&((TSmth*)arg)->mutex);
-  pthread_exit(NULL);
-}
 
-void* equal(void* arg) {
-  pthread_mutex_lock(&((TSmth*)arg)->mutex);
-  
+  }
   for (int i = 0; i < ((TSmth*)arg)->iterations; i += 2) {
     if (((TSmth*)arg)->deck[i] == ((TSmth*)arg)->deck[i + 1])
-      ((TSmth*)arg)->equalCount++;
+      ((TSmth*)arg)->isEqual++;
   }
   
-  pthread_mutex_unlock(&((TSmth*)arg)->mutex);
   pthread_exit(NULL);
 }
 
+
 int main(int argc, char* argv[]) {
-  pthread_t cardsShuffle, cardsEqual;
-  TSmth deck;
-  deck.iterations = atoi(argv[1]);
-  deck.equalCount = 0;
-  deck.deck = (int*)malloc(sizeof(int) * deck.iterations * 2);
-
-  if (pthread_mutex_init(&deck.mutex, NULL)) {
-    perror("pthread_mutex_init");
-    exit(-1);
+  if (argc == 1 || argc == 2 || argc > 3) {
+    printf("Usage: one ExperimentsNumber ThreadsNumber\n");
+    exit(0);
   }
-  if (pthread_create(&cardsShuffle, NULL, randr, (void*)&deck)) {
-    perror("pthread_create(cardsShuffle)");
-    exit(-1);
-  }
-  if (pthread_create(&cardsEqual, NULL, equal, (void*)&deck)) {
-    perror("pthread_create(cardsEqual)");
-    exit(-1);
-  }
-
-  pthread_join(cardsShuffle, NULL);
-  pthread_join(cardsEqual, NULL);
-  printf("Chance is %f\n", deck.equalCount / (deck.iterations * 1.0));
   
-  pthread_mutex_destroy(&deck.mutex);
-  free(deck.deck);
-  pthread_exit(NULL);
+  int iterations = atoi(argv[1]);
+  int threads = atoi(argv[2]);
+  double result = 0;
+  if (iterations < 0 || threads < 0 ||
+      threads == 0 || iterations == 0) {
+    printf("Change your numbers dumb4SS\n");
+    exit(-1);
+  }
+  if (iterations <= threads) {
+    TSmth arr[iterations];
+    pthread_t process[iterations];
+    for (int i = 0; i < iterations; i++) {
+      arr[i].deck = malloc(sizeof(int) * 2);
+      arr[i].isEqual = 0;
+      arr[i].iterations = 2;
+      
+      if (pthread_create(&process[i], NULL, randr, (void*)&arr[i])) {
+        perror("pthread_create");
+        exit(-1);
+      }
+
+    }
+
+    for (int i = 0; i < iterations;i++) {
+      result += arr[i].isEqual;
+      free(arr[i].deck);
+    }
+    printf("Chance is %f\n", result / iterations);
+
+  } else {
+    TSmth arr[threads];
+    pthread_t process[threads];
+
+    for (int i = 0; i < threads; i++) {
+      arr[i].deck = malloc(sizeof(int) * 2 * (iterations / threads));
+      arr[i].isEqual = 0;
+      arr[i].iterations = 2 * (iterations / threads);
+      
+      if (pthread_create(&process[i], NULL, randr, (void*)&arr[i])) {
+        perror("pthread_create");
+        exit(-1);
+      }
+
+    }
+
+    for (int i = 0; i < threads;i++) {
+      result += arr[i].isEqual;
+      free(arr[i].deck);
+    }
+    printf("Chance is %f\n", result / iterations);
+    
+  }  
+  
+  exit(0);
 }
