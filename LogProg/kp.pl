87e1a84696a90bdd4814cd7fdb350418499482b7
mehakun <mehakun1@gmail.com>
2016-12-19 22:37:50 +0300

    deleted trash

--- LogProg/kp.pl
+++
@@ -1,2 +0,0 @@
-brother(Y,X):-parent(Z,Y),parent(Z,X),sex(Y, m),X<>Y.
-brotherInSquare(Y,X):-parent(Z,X),brother(Z,F),parent(F,Y),sex(Y, m).
--- LogProg/kp/kp.pl
+++
@@ -1,2 +0,0 @@
-brother(Y,X):-parents(Z,Y),parents(Z,X),sex(Y, m),X<>Y.
-brotherInSquare(Y,X):-parents(Z,X),brother(Z,F),parents(F,Y),sex(Y, m).
